6cb8720c19c9c9dbfff48f411150893be5a3a945
Cleber-0101 <cleberbatista99@hotmail.com>
2024-10-01 13:25:18 -0300

    Praticando operadores

--- fundamentos/OperadoresRelacionais.cs
+++ fundamentos/OperadoresRelacionais.cs
@@ -10,7 +10,14 @@ namespace CursoCSharp.fundamentos
     {
         public static void Excecutar()
         {
-            
+            double nota = 6.0;
+            double notapraPassardeAno = 7.0;
+
+            Console.WriteLine("passou de ano ? ", nota < 7); // ele não passou de ano 
+            Console.WriteLine("não passou de ano" , notapraPassardeAno > nota); //ele não passou de ano Porque a nota para passar é maior que a nota do Aluno 
+            Console.WriteLine("Como foi sua nota" , nota != notapraPassardeAno); //existe diferença 
+            Console.WriteLine("Sua nota é igual a nota de corte ?" , nota == notapraPassardeAno); //então certamente ele passou de ano 
+                                                                                                 
         }
     }
 }
